e75eac27a43e6c01547afcf23ff807c15d7e4ef8
sirhmvfx <sirhmvfx@gmail.com>
2023-01-10 19:48:15 +0100

    [fix] - navigation

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function() {
+hamburger.onclick = function () {
     const menu = document.querySelector(".menu");
     menu.classList.toggle("is-active");
+    hamburger.classList.toggle("is-active");
 }
+
+
+
+
+// const hamburger = document.querySelector(".hamburger");
+// const menu = document.querySelector(".menu");
+
+// function addMenu (e ){
+//     if(e.target.click){
+//         menu.classList.toggle('dispNone')
+//     }
+// }
+
+// hamburger.addEventListener('click', addMenu)
